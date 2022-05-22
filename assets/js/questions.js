29c9173c37ffcd79900d0328f1bd84f44dd07229
DominikBBB <dominik.brzeski@wp.eu>
2022-05-22 18:20:30 +0000

    Add 10 quiz questions with answers

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,16 +1,73 @@
+[
 {
-    "question": "",
-    "a1": "",
-    "a2": "",
-    "a3": "",
-    "a4": "",
-    "answer": ""
+    "question": "When was the Saxon Palace built?",
+    "a1": "1639-1642",
+    "a2": "1611",
+    "a3": "2022",
+    "a4": "1945",
+    "answer": "a1"
 }, {
-    "question": "",
-    "a1": "",
-    "a2": "",
-    "a3": "",
-    "a4": "",
-    "answer": ""
+    "question": "Where is the Saxon Palace located?",
+    "a1": "Gdansk",
+    "a2": "London",
+    "a3": "No-where",
+    "a4": "Warsaw",
+    "answer": "a3"
+}, {
+    "question": "When will the Saxon Palace be restorated?",
+    "a1": "1945",
+    "a2": "2100",
+    "a3": "2023-2030",
+    "a4": "2018",
+    "answer": "a3"
+}, {
+    "question": "Which famous pianist was living in the Saxon Palace?",
+    "a1": "Chopin",
+    "a2": "Bach",
+    "a3": "Vivaldi",
+    "a4": "Vangelis",
+    "answer": "a1"
+}, {
+    "question": "When was the Saxon Palace destroyed by Germans?",
+    "a1": "1918",
+    "a2": "1900",
+    "a3": "1989",
+    "a4": "1945",
+    "answer": "a4"
+}, {
+    "question": "What is located on the Saski Square now?",
+    "a1": "Ice-cream truck",
+    "a2": "Tomb of the Unknown Soldier",
+    "a3": "Coffee Shop",
+    "a4": "Nothing",
+    "answer": "a2"
+}, {
+    "question": "What was located NEXT to the Saxon Palace?",
+    "a1": "Grocery Shop",
+    "a2": "Brühl Palace",
+    "a3": "Airport",
+    "a4": "Coding Department",
+    "answer": "a2"
+}, {
+    "question": "What was located IN the Saxon Palace?",
+    "a1": "Grocery Shop",
+    "a2": "Brühl Palace",
+    "a3": "Airport",
+    "a4": "Coding Department",
+    "answer": "a4"
+}, {
+    "question": "Which army was operated in the Saxon Palace in 1918-1939?",
+    "a1": "The Polish Army",
+    "a2": "The German Army",
+    "a3": "The Russian Army",
+    "a4": "NATO",
+    "answer": "a1"
+}, {
+    "question": "What architectural concept is conected to the Saxon Palace?",
+    "a1": "Warsaw's water pipes system",
+    "a2": "Saxon Axis with the Garden, Theatre and the Iron Gate",
+    "a3": "None",
+    "a4": "Love Parade tents",
+    "answer": "a2"
 },
-
+]
