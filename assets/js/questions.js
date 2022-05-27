5e3a73022dfb7bca1b0227cb04a39d984761c0bd
DominikBBB <dominik.brzeski@wp.eu>
2022-05-27 18:47:09 +0000

    Add quiz sections in html, style container and quiz sections, add variables for home and rules pages, add event listener to submit button on home page

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -20,54 +20,55 @@ let questions = [
     "a3": "2023-2030",
     "a4": "2018",
     "answer": "3"
-}, {
-    "question": "Which famous pianist was living in the Saxon Palace?",
-    "a1": "Chopin",
-    "a2": "Bach",
-    "a3": "Vivaldi",
-    "a4": "Mozart",
-    "answer": "1"
-}, {
-    "question": "When was the Saxon Palace destroyed by Germans?",
-    "a1": "1918",
-    "a2": "1900",
-    "a3": "1989",
-    "a4": "1945",
-    "answer": "4"
-}, {
-    "question": "What is located on the Saski Square now?",
-    "a1": "Ice-cream truck",
-    "a2": "Tomb of the Unknown Soldier",
-    "a3": "Coffee Shop",
-    "a4": "Nothing",
-    "answer": "2"
-}, {
-    "question": "What was located NEXT to the Saxon Palace?",
-    "a1": "Grocery Shop",
-    "a2": "Brühl Palace",
-    "a3": "Airport",
-    "a4": "Coding Department",
-    "answer": "2"
-}, {
-    "question": "What was located IN the Saxon Palace?",
-    "a1": "Grocery Shop",
-    "a2": "Brühl Palace",
-    "a3": "Airport",
-    "a4": "Coding Department",
-    "answer": "4"
-}, {
-    "question": "Which army was operated in the Saxon Palace in 1918-1939?",
-    "a1": "The Polish Army",
-    "a2": "The German Army",
-    "a3": "The Russian Army",
-    "a4": "NATO",
-    "answer": "1"
-}, {
-    "question": "What architectural concept is connected to the Saxon Palace?",
-    "a1": "Warsaw's water pipes system",
-    "a2": "Saxon Axis with the Garden, Theatre and the Iron Gate",
-    "a3": "None",
-    "a4": "Love Parade tents",
-    "answer": "2"
 },
+// {
+//     "question": "Which famous pianist was living in the Saxon Palace?",
+//     "a1": "Chopin",
+//     "a2": "Bach",
+//     "a3": "Vivaldi",
+//     "a4": "Mozart",
+//     "answer": "1"
+// }, {
+//     "question": "When was the Saxon Palace destroyed by Germans?",
+//     "a1": "1918",
+//     "a2": "1900",
+//     "a3": "1989",
+//     "a4": "1945",
+//     "answer": "4"
+// }, {
+//     "question": "What is located on the Saski Square now?",
+//     "a1": "Ice-cream truck",
+//     "a2": "Tomb of the Unknown Soldier",
+//     "a3": "Coffee Shop",
+//     "a4": "Nothing",
+//     "answer": "2"
+// }, {
+//     "question": "What was located NEXT to the Saxon Palace?",
+//     "a1": "Grocery Shop",
+//     "a2": "Brühl Palace",
+//     "a3": "Airport",
+//     "a4": "Coding Department",
+//     "answer": "2"
+// }, {
+//     "question": "What was located IN the Saxon Palace?",
+//     "a1": "Grocery Shop",
+//     "a2": "Brühl Palace",
+//     "a3": "Airport",
+//     "a4": "Coding Department",
+//     "answer": "4"
+// }, {
+//     "question": "Which army was operated in the Saxon Palace in 1918-1939?",
+//     "a1": "The Polish Army",
+//     "a2": "The German Army",
+//     "a3": "The Russian Army",
+//     "a4": "NATO",
+//     "answer": "1"
+// }, {
+//     "question": "What architectural concept is connected to the Saxon Palace?",
+//     "a1": "Warsaw's water pipes system",
+//     "a2": "Saxon Axis with the Garden, Theatre and the Iron Gate",
+//     "a3": "None",
+//     "a4": "Love Parade tents",
+//     "answer": "2"
+// },
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {});
+
+let homeEl = document.getElementById('home');
+let playerNameEl = document.getElementById('playername');
+let form = document.getElementById('playername-form');
+
+let startButtonEl = document.getElementById('quiz-start');
+let rulesEl = document.getElementById('rules');
+let playerEl = document.getElementById('player');
+let quizEl = document.getElementById('quiz');
+
+
+
+form.addEventListener('submit', function(event) {
+    homeEl.style.display = 'none';
+    rulesEl.classList.remove('hide-content');
+    playerEl.innerHTML = "Hi " + playerNameEl.value;
+    event.preventDefault();
+});
+
+
+startButtonEl.addEventListener('click', function() {
+    rulesEl.classList.add('hide-content');
+    quizEl.classList.remove('hide-content');
+});
+
+
+
+
+let totalQuestions = questions.length;
 let currentQuestion = 0;
 let score = 0;
-let totalQuestions = questions.length;
 
 let container = document.getElementById('quizContainer');
 let questionElement = document.getElementById('question');
