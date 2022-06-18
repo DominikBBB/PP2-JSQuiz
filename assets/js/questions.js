cab72daca640b5407acba91d55969c376cd8a6a8
DominikBBB <dominik.brzeski@wp.eu>
2022-06-18 23:34:14 +0000

    Unlock all questions for testing

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,41 +3,41 @@ let questionBank = [{
         option: ["1639-1642", "1611", "2022", "1945"],
         answer: "1639-1642"
     }, {
-        question: "Where IS the Saxon Palace located?",
+        question: "Where IS the Saxon Palace located now?",
         option: ["Gdansk", "London", "No-where. It's destroyed.", "Warsaw"],
         answer: "No-where. It's destroyed."
     }, {
-        question: "When will the Saxon Palace be restorated?",
+        question: "When will the Saxon Palace be restored?",
         option: ["1945", "2100", "2023-2030", "2018"],
         answer: "2023-2030"
     },
-    // {
-    //     question : "Which famous pianist was living in the Saxon Palace?",
-    //     option : ["Chopin", "Bach", "Vivaldi", "Mozart"],
-    //     answer : "Chopin"
-    // }, {
-    //     question : "When was the Saxon Palace destroyed by Germans?",
-    //     option : ["1918", "1900", "1989", "1945"],
-    //     answer: "1945"
-    // }, {
-    //     question : "What is located on the Saski Square now?",
-    //     option : ["Ice-cream truck", "Tomb of the Unknown Soldier", "Coffee Shop", "Nothing"],
-    //     answer: "Tomb of the Unknown Soldier"
-    // }, {
-    //     question : "What was located NEXT to the Saxon Palace?",
-    //     option : ["Grocery Shop", "Brühl Palace", "Airport", "Coding Department"],
-    //     answer: "Brühl Palace"
-    // }, {
-    //     question : "What was located IN the Saxon Palace?",
-    //     option : ["Grocery Shop", "Brühl Palace", "Airport", "Coding Department"],
-    //     answer: "Coding Department"
-    // }, {
-    //     question : "Which army was operated in the Saxon Palace in 1918-1939?",
-    //     option : ["The Polish Army", "The German Army", "The Russian Army", "NATO"],
-    //     answer: "The Polish Army"
-    // }, {
-    //     question : "What architectural concept is connected to the Saxon Palace?",
-    //     option : ["Warsaw's water pipes system", "Saxon Axis with the Garden, Theatre and the Iron Gate", "None", "Love Parade tents"],
-    //     answer: "Saxon Axis with the Garden, Theatre and the Iron Gate"
-    // },
+    {
+        question : "Which famous pianist was living in the Saxon Palace?",
+        option : ["Chopin", "Bach", "Vivaldi", "Mozart"],
+        answer : "Chopin"
+    }, {
+        question : "When was the Saxon Palace destroyed by Germans?",
+        option : ["1918", "1900", "1989", "1945"],
+        answer: "1945"
+    }, {
+        question : "What is located on the Saski Square now?",
+        option : ["Ice-cream truck", "Tomb of the Unknown Soldier", "Coffee Shop", "Nothing"],
+        answer: "Tomb of the Unknown Soldier"
+    }, {
+        question : "What was located NEXT to the Saxon Palace?",
+        option : ["Grocery Shop", "Brühl Palace", "Airport", "Coding Department"],
+        answer: "Brühl Palace"
+    }, {
+        question : "What was located IN the Saxon Palace?",
+        option : ["Grocery Shop", "Brühl Palace", "Airport", "Coding Department"],
+        answer: "Coding Department"
+    }, {
+        question : "Which army was operated in the Saxon Palace in 1918-1939?",
+        option : ["The Polish Army", "The German Army", "The Russian Army", "NATO"],
+        answer: "The Polish Army"
+    }, {
+        question : "What architectural concept is connected to the Saxon Palace?",
+        option : ["Warsaw's water pipes system", "Saxon Axis with the Garden, Theatre and the Iron Gate", "None", "Love Parade tents"],
+        answer: "Saxon Axis with the Garden, Theatre and the Iron Gate"
+    },
 ]
